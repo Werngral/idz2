44d40bc76618108f0aa8eca173f2b4bedc254c16
Werngral <86944051+Werngral@users.noreply.github.com>
2024-10-18 22:39:50 +1000

    Add files via upload

---
+++ dz2(2).pas
@@ -0,0 +1,16 @@
+﻿
+var
+  a1,a2,a4,a8,a13:real;
+  f:text;
+begin
+  assign(f,'C:\Program Files (x86)\PascalABC.NET\Files\in2.dat');
+  reset(f);
+  read(f,a1);
+  writeln('Число a=',a1);
+  a2:=a1*a1;
+  a4:=a2*a2;
+  a8:=a4*a4;
+  a13:=a8*a4*a1;
+  writeln('Результат: ',a13);
+  
+end.
---
+++ dz2(3).pas
@@ -0,0 +1,27 @@
+﻿
+var
+  p,ya,yb,yc,a,b,c,r1,r2,s:real;
+  f:text;
+begin
+  assign(f,'C:\Program Files (x86)\PascalABC.NET\Files\in3.dat');
+  reset(f);
+  read(f,yb);
+  read(f,yc);
+  read(f,a);
+  writeln(yb);
+  writeln(yc);
+  writeln(a);
+  ya:=180-yb-yc;
+  b:=a*sin(yb*pi/180)/sin(ya*pi/180);
+  c:=a*sin(yc*pi/180)/sin(ya*pi/180);
+  p:=(a+b+c)/2;
+  s:=0.5*c*((a*b)/c);
+  r1:=s/p;
+  r2:=a*2*sin(ya*pi/180);
+  close(f);
+  rewrite(f);
+  writeln(f,r1);
+  writeln(f,r2);
+  close(f);
+  
+end.
